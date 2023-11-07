dbac5cd2eefeafc269f259f194fe39ddaaf9f0fd
Eshwar1807 <141611885+Eshwar1807@users.noreply.github.com>
2023-11-07 14:16:06 +0530

    Add files via upload

---
+++ python interface/jaffir sabari basith.py
@@ -0,0 +1,17 @@
+
+
+import sim
+import sys
+import tkinter
+print ('Program started')
+sim.simxFinish(-1) # just in case, close all opened connections
+clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
+if clientID!=-1:
+    print ('Connected to remote API server')
+
+    # Now try to retrieve data in a blocking fashion (i.e. a service call):
+    res,objs=sim.simxGetObjects(clientID,sim.sim_handle_all,sim.simx_opmode_blocking)
+    if res==sim.simx_return_ok:
+        print ('Number of objects in the scene: ',len(objs))
+    else:
+        print ('Remote API function call returned with error code: ',res)
